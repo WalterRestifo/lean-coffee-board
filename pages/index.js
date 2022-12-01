c0c1cbb2374637e699ba9e30d0846d622f7893fc
Walter Restifo <116001970+WalterRestifo@users.noreply.github.com>
2022-12-01 11:59:43 +0100

    complete task 2 (#1)

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
+import Form from "../components/Form";
+import Header from "../components/Header";
+import { useState } from "react";
+import Card from "../components/Card";
+
 export default function HomePage() {
+  const [list, setList] = useState([
+    {
+      thought: "will I work correctly?",
+      author: "Walter",
+    },
+    {
+      thought: "I hope so",
+      author: "wieder Walter",
+    },
+  ]);
+
+  function handleAddNewListObj(newListObj) {
+    setList([...list, newListObj]);
+  }
+
   return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
+    <>
+      <Header />
+      <ul>
+        {list.map(({ thought, author }) => {
+          return <Card key={thought} thought={thought} author={author} />;
+        })}
+      </ul>
+      <hr />
+      <Form onAddNewListObj={handleAddNewListObj} />
+    </>
   );
 }
