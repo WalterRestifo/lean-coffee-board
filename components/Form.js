adcef572db8ae3fb828706c8c4de14cc6b716a23
Walter Restifo <116001970+WalterRestifo@users.noreply.github.com>
2022-12-01 15:54:12 +0100

    Feature/task3 (#2)
    * complete task 3
    
    * add styles
    
    * letzter commit

--- components/Form.js
+++ components/Form.js
@@ -10,22 +10,51 @@ export default function Form({ onAddNewListObj }) {
     e.preventDefault();
     onAddNewListObj(newListObj);
     e.target.reset();
+    e.target.thoughts.focus();
   }
 
   return (
     <StyledForm onSubmit={handleSubmit}>
-      <input name="thoughts" placeholder="Type your thoughts..."></input>
-      <input name="author" placeholder="Your name"></input>
-      <StyledButton>+</StyledButton>
+      <input
+        aria-label="Type your thoughts..."
+        name="thoughts"
+        placeholder="Type your thoughts..."
+        required
+      ></input>
+      <input
+        aria-label="Your name"
+        name="author"
+        placeholder="Your name"
+        required
+      ></input>
+      <StyledButton aria-label="add new card">+</StyledButton>
     </StyledForm>
   );
 }
 
 const StyledForm = styled.form`
+  position: sticky;
+  bottom: 0;
+
+  border: 1px solid black;
+  min-height: 7vh;
+
+  padding-top: 2vh;
+  padding-bottom: 2vh;
+
+  background-color: #120905;
+  color: #a2826a;
+
   display: flex;
   justify-content: space-around;
 `;
 
 const StyledButton = styled.button`
   border-radius: 25px;
+  background-color: #403e25;
+  color: #a2826a;
+
+  &:hover {
+    background-color: #263829;
+  }
 `;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Form from "../components/Form";
 import Header from "../components/Header";
 import { useState } from "react";
 import Card from "../components/Card";
+import styled from "styled-components";
 
 export default function HomePage() {
   const [list, setList] = useState([
@@ -11,7 +12,7 @@ export default function HomePage() {
     },
     {
       thought: "I hope so",
-      author: "wieder Walter",
+      author: "Walter again",
     },
   ]);
 
@@ -19,16 +20,38 @@ export default function HomePage() {
     setList([...list, newListObj]);
   }
 
+  function handleRemoveListObj(ListObj) {
+    const newList = list.filter(({ thought, author }) => {
+      return !(thought === ListObj.thought && author === ListObj.author);
+    });
+    setList(newList);
+  }
+  console.log(list);
   return (
-    <>
+    <StyledDiv>
       <Header />
-      <ul>
+      <StyledUl>
         {list.map(({ thought, author }) => {
-          return <Card key={thought} thought={thought} author={author} />;
+          return (
+            <Card
+              key={thought + author}
+              thought={thought}
+              author={author}
+              onRemoveListObj={handleRemoveListObj}
+            />
+          );
         })}
-      </ul>
-      <hr />
+      </StyledUl>
       <Form onAddNewListObj={handleAddNewListObj} />
-    </>
+    </StyledDiv>
   );
 }
+
+const StyledUl = styled.ul`
+  height: 100vh;
+`;
+
+const StyledDiv = styled.div`
+  position: relative;
+  background-color: #665e48;
+`;
